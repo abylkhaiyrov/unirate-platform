aa0dffc6b72f93c0ba369e49fe090931563a8a34
abylkhaiyrov <askar.abylkhaiyrov.00@gmail.com>
2025-02-10 22:09:22 +0500

    add new micro-services for university platform

---
+++ unirate-platform-university/src/main/java/kz/abylkhaiyrov/unirateplatformuniversity/UniratePlatformUniversityApplication.java
@@ -0,0 +1,13 @@
+package kz.abylkhaiyrov.unirateplatformuniversity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniratePlatformUniversityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniratePlatformUniversityApplication.class, args);
+    }
+
+}
