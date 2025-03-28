ee38d58df42a1ca93a5078cac043e9ca3304bce5
Askar Abylkhaiyrov <askar.abylkhaiyrov.00@gmail.com>
2025-03-28 12:00:30 +0500

    comment cors config

--- unirate-platform-university/src/main/java/kz/abylkhaiyrov/unirateplatformuniversity/configuration/WebConfig.java
+++ unirate-platform-university/src/main/java/kz/abylkhaiyrov/unirateplatformuniversity/configuration/WebConfig.java
@@ -1,19 +1,19 @@
-package kz.abylkhaiyrov.unirateplatformuniversity.configuration;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("https://unirate.kz", "https://www.unirate.kz")
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*")
-                .allowCredentials(true);
-    }
-
-}
+//package kz.abylkhaiyrov.unirateplatformuniversity.configuration;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                .allowedOrigins("https://unirate.kz", "https://www.unirate.kz")
+//                .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                .allowedHeaders("*")
+//                .allowCredentials(true);
+//    }
+//
+//}
